6920ab8afb82bad792d5f35785dd9a69f1c3e2cb
ujangracinggg <juangketek@GMAIL.COM>
2024-12-11 07:37:11 +0700

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,4 @@
+// JavaScript bisa ditambahkan untuk menambah interaktivitas lebih lanjut
+document.addEventListener("DOMContentLoaded", () => {
+    // Bisa menambahkan fungsi JavaScript interaktif jika diperlukan di sini
+});
